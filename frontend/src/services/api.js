2d92f4983b2d29e1ce76b76a53da5261f2e21e33
Indo86 <deninorman77@gmail.com>
2025-05-28 09:03:21 +0700

    penambahan package

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:5000/api'
+const API_URL = 'https://buku-tukar-559917148272.us-central1.run.app'
 
 // axios instance
 const apiInstance = axios.create({
